8256e5231124ad8d61bdbd1fa5e96f4a1b660769
Gina González <gini_go@hotmail.com>
2024-07-16 17:21:56 +0200

    add favorite results in javascript

--- main.js
+++ main.js
@@ -3,8 +3,26 @@
 const buttonSearch = document.querySelector(".js-button-search");
 const inputSearch = document.querySelector(".js-input-search");
 const results = document.querySelector(".js-results");
+let series = [];
+const favoriteSeriesList = [];
+const FavoriteSeriesDiv = document.querySelector(".js-favorites-series");
 
-const handleClick = (event) => {
+const renderSeries = (list, div) => {
+  div.innerHTML = "";
+  for (const serie of list) {
+    div.innerHTML += `<div class="js-serie" id="${serie.mal_id}"> 
+   <h1>${serie.title}</h1> 
+   <img src="${serie.images.jpg.large_image_url}" alt="${serie.title}">
+  </div>`;
+  }
+
+  const seriesDom = document.querySelectorAll(".js-serie");
+  for (const serieDom of seriesDom) {
+    serieDom.addEventListener("click", handleClickSerie);
+  }
+};
+
+const handleClickSearch = (event) => {
   event.preventDefault();
   const searchString = inputSearch.value;
   fetch(`https://api.jikan.moe/v4/anime?q=${searchString}`)
@@ -12,16 +30,25 @@ const handleClick = (event) => {
       return response.json();
     })
     .then((data) => {
-      const series = data.data;
-      console.log(series);
-      for (const serie of series) {
-        results.innerHTML += `<div> 
-         <h1>${serie.title}</h1> 
-         <img src="${serie.images.jpg.large_image_url}" alt="${serie.title}">
-        </div>`;
-        console.log(serie);
-      }
+      series = data.data;
+      renderSeries(series, results);
     });
 };
 
-buttonSearch.addEventListener("click", handleClick);
+buttonSearch.addEventListener("click", handleClickSearch);
+
+function handleClickSerie(event) {
+  const serieClickId = event.currentTarget.id;
+  const serieSelect = series.find((serie) => {
+    return serie.mal_id === parseInt(serieClickId);
+  });
+  const indexSeriesinFavorite = favoriteSeriesList.findIndex(
+    (favoriteSerie) => {
+      return favoriteSerie.mal_id === parseInt(serieClickId);
+    }
+  );
+  if (indexSeriesinFavorite === -1) {
+    favoriteSeriesList.push(serieSelect);
+    renderSeries(favoriteSeriesList, FavoriteSeriesDiv);
+  }
+}
